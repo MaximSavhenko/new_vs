92920289fc54caf40215717fc201f4053d722a61
maximsavhenko <uspmsm@gmail.com>
2020-12-08 22:44:34 +0200

    add udate

--- src/assets/js/slider.js
+++ src/assets/js/slider.js
@@ -9,8 +9,18 @@ $(document).ready(function(){
 
 $(document).ready(function(){
     $('.product__previews-slider').slick({
+      centerMode: true,
       slidesToShow: 1,
+      arrows: true,
+      infinite: false,
+    });
+  });
+
+$(document).ready(function(){
+    $('.product__viewed-slider').slick({
+      slidesToShow: 4,
       slidesToScroll: 1,
       arrows: true,
+      infinite: !1
     });
   });
