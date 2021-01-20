a05ac4830a8719734a283c3ce778afe32d16262a
maximsavhenko <uspmsm@gmail.com>
2021-01-20 17:16:28 +0200

    fix bugs on main page and product page

--- src/assets/js/jQuery.js
+++ src/assets/js/jQuery.js
@@ -3,6 +3,14 @@ $(document).ready(function () {
     $(this).toggleClass("active");
   });
 
+  $("#active-360").click(function () {
+    $(".product__slider-review").addClass("active");
+  });
+  
+  $("#close-360").click(function () {
+    $(".product__slider-review").removeClass('active');
+  });
+
   $(".product__responsive-title").click(function () {
     $(".product__responsive-title").not(this).removeClass('activeArrow');
     $(this).toggleClass("activeArrow");
